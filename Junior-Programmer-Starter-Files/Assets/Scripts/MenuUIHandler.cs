c43b383f1d16c8f6938965066c0faeb78c4d9f31
VickyOct <103393125+VickyOct@users.noreply.github.com>
2024-03-05 16:41:32 -0500

    Create SaveData class and implement data flow between scenes

--- Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
@@ -24,6 +24,17 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -32,6 +43,7 @@ public class MenuUIHandler : MonoBehaviour
     }   
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
